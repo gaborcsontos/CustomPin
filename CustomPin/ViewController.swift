d936723a06017cb3b9d539ea5080e24fbb9cd6c0
Gabor <acsontosgabor@gmail.com>
2015-11-25 16:03:04 +0000

    Initial Commit

---
+++ CustomPin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomPin
+//
+//  Created by Gabor Csontos on 11/25/15.
+//  Copyright © 2015 Gabor Csontos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
